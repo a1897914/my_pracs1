ed2b93e6f583234c342dec6abb4f1db3bc3f5e16
a1897914 <140685907+a1897914@users.noreply.github.com>
2023-09-08 17:50:10 +0930

    Add files via upload

--- main-2-2.cpp
+++ main-2-2.cpp
@@ -8,16 +8,16 @@ int main() {
     std::cin >> fuel;
     a.set_fuel(fuel);
 
+    float currentFuel = a.get_fuel();
+    std::cout << "Current fuel: " << currentFuel << std::endl;
+
 
     std::cout << "Weight: " << a.get_weight() << std::endl;
 
     std::cout << "Number of Flights: " << a.get_numberOfFlights() << std::endl;
 
-    a.refuel();
-    std::cout << "Fuel: " << a.get_fuel() << "%" << std::endl;
-
     a.fly(65, 120);
-    std::cout << "Fuel after flying: " << a.get_fuel() << std::endl;
+    std::cout << "Fuel after flying: " << a.get_fuel() << "%" << std::endl;
     std::cout << "Number of Flights after flying: " << a.get_numberOfFlights() << std::endl;
 
     return 0;
