2b4ce55ff9dd8c3a75abc53483e439340ab7d966
a1897914 <140685907+a1897914@users.noreply.github.com>
2023-09-08 17:33:38 +0930

    Add files via upload

--- main-2-1.cpp
+++ main-2-1.cpp
@@ -7,15 +7,14 @@ int main() {
     std::cout << "Enter fuel value: ";
     std::cin >> fuel;
     h.set_fuel(fuel);
-    std::cout << "Fuel set to: " << h.get_fuel() << std::endl;
+    std::cout << "Fuel: " << h.get_fuel() << std::endl;
 
 
     std::cout << "Weight: " << h.get_weight() << std::endl;
-    std::cout << "Fuel: " << h.get_fuel() << std::endl;
     std::cout << "Number of Flights: " << h.get_numberOfFlights() << std::endl;
 
     h.refuel();
-    std::cout << "Fuel after refueling: " << h.get_fuel() << "%" << std::endl;
+    std::cout << "Fuel : " << h.get_fuel() << "%" << std::endl;
 
     h.fly(45, 10);
     std::cout << "Fuel after flying: " << h.get_fuel() << std::endl;
--- main-2-2.cpp
+++ main-2-2.cpp
@@ -7,16 +7,14 @@ int main() {
     std::cout << "Enter fuel value: ";
     std::cin >> fuel;
     a.set_fuel(fuel);
-    std::cout << "Fuel set to: " << a.get_fuel() << std::endl;
-    
-    std::cout << "Weight: " << a.get_weight() << std::endl;
-
     std::cout << "Fuel: " << a.get_fuel() << std::endl;
+
+    std::cout << "Weight: " << a.get_weight() << std::endl;
 
     std::cout << "Number of Flights: " << a.get_numberOfFlights() << std::endl;
 
     a.refuel();
-    std::cout << "Fuel after refueling: " << a.get_fuel() << "%" << std::endl;
+    std::cout << "Fuel: " << a.get_fuel() << "%" << std::endl;
 
     a.fly(65, 120);
     std::cout << "Fuel after flying: " << a.get_fuel() << std::endl;
