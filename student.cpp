c69a97869af69406a477f709041f6f1c724f1e97
a1897914 <140685907+a1897914@users.noreply.github.com>
2023-09-12 14:26:42 +0930

    Add files via upload

--- student.cpp
+++ student.cpp
@@ -6,4 +6,14 @@ Student::Student(const std::string& name, int age, const std::string& email, con
     // Initialize any additional attributes specific to students if needed
 }
 
+// Getter for studentID
+std::string Student::getStudentID() const {
+    return studentID;
+}
+
+// Setter for studentID
+void Student::setStudentID(const std::string& newStudentID) {
+    studentID = newStudentID;
+}
+
 // You can define additional methods specific to students here
